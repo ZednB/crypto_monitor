97100a5b08652e9958ef64064f80b8e044441b36
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2024-09-27 12:49:54 +0300

    Start project

---
+++ app/config.py
@@ -0,0 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_URL = os.getenv("DB_URL")
+
+TORTOISE_ORM = {
+    "connections": {
+        "default": DB_URL
+    },
+    "apps": {
+        "models": {
+            "models": ["app.models", "aerich.models"],
+            "default_connection": "default",
+        }
+    }
+}
